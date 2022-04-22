0a753a1ea9626058ec4158acc17ec1237e0fd153
SunshineNurSmile <yuanyiw@bu.edu>
2022-04-21 22:59:19 -0400

    Co-authored-by: kuiduo7 <kuiduo7@users.noreply.github.com>

--- js/trainee/connection/plantiers.js
+++ js/trainee/connection/plantiers.js
@@ -59,41 +59,14 @@ var app = new Vue({
             })
         },
 
+        jump_to_pp(price) {
+            window.localStorage.setItem('price', price);
+            window.localStorage.setItem('trainer_id', this.trainer_id);
+            window.location.href = '/html/trainee/connection/tierdetail.html';
+        },
+
         back() {
             window.location.href = '/html/trainee/connection/trainercard.html?trainer_id=' + this.trainer_id;
         }
     }
 })
-
-// Render the PayPal button into #paypal-button-container
-paypal.Buttons({
-
-    // Set up the transaction
-    createOrder: function(data, actions) {
-        return actions.order.create({
-            purchase_units: [{
-                amount: {
-                    value: '88.44'
-                }
-            }]
-        });
-    },
-
-    // Finalize the transaction
-    onApprove: function(data, actions) {
-        return actions.order.capture().then(function(orderData) {
-            // Successful capture! For demo purposes:
-            console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
-            var transaction = orderData.purchase_units[0].payments.captures[0];
-            alert('Transaction '+ transaction.status + ': ' + transaction.id + '\n\nSee console for all available details');
-
-            // Replace the above to show a success message within this page, e.g.
-            // const element = document.getElementById('paypal-button-container');
-            // element.innerHTML = '';
-            // element.innerHTML = '<h3>Thank you for your payment!</h3>';
-            // Or go to another URL:  actions.redirect('thank_you.html');
-        });
-    }
-
-
-}).render('#paypal-button-container');
--- js/trainee/connection/tierdetail.js
+++ js/trainee/connection/tierdetail.js
@@ -1,35 +1,127 @@
-// Render the PayPal button into #paypal-button-container
-paypal.Buttons({
+const PayPalButton = paypal.Buttons.driver("vue", window.Vue);
 
-    // Set up the transaction
-    createOrder: function(data, actions) {
-        return actions.order.create({
-            intent: 'CAPTURE',
-            purchase_units: [{
-                amount: {
-                    currency_code: 'USD',
-                    value: '220.00'
-                },
-                payee: { 
-                    email_address: 'payless@gmail.com'
-                }
-            }]
-        });
+Vue.component("app", {
+    // The style prop for the PayPal button should be passed in as `style-object` or `styleObject` to avoid conflict with Vue's `style` reserved prop.
+    template: `
+        <paypal-buttons :on-approve="onApprove" :create-order="createOrder" :on-shipping-change="onShippingChange" :on-error="onError" :style-object="style" />
+    `,
+    components: {
+        "paypal-buttons": PayPalButton,
     },
 
-    // Finalize the transaction
-    onApprove: function(data, actions) {
-        return actions.order.capture().then(function(orderData) {
-            // Successful capture! For demo purposes:
-            console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
-            var transaction = orderData.purchase_units[0].payments.captures[0];
-            alert('Transaction '+ transaction.status + ': ' + transaction.id + '\n\nSee console for all available details');
+    computed: {
+        createOrder: function () {
+            var price = window.localStorage.getItem('price');
+            console.log(price);
+            return (data, actions) => {
+                return actions.order.create({
+                    purchase_units: [
+                        {
+                            amount: {
+                                value: price,
+                            },
+                        },
+                    ],
+                });
+            }
+        },
 
-            // Replace the above to show a success message within this page, e.g.
-            // const element = document.getElementById('paypal-button-container');
-            // element.innerHTML = '';
-            // element.innerHTML = '<h3>Thank you for your payment!</h3>';
-            // Or go to another URL:  actions.redirect('thank_you.html');
-        });
+        onApprove: function () {
+            return (data, actions) => {
+                window.localStorage.setItem('orderID', data.orderID);
+                return actions.order.capture();
+            }
+        },
+
+        onShippingChange: function () {
+            return (data, actions) => {
+                if (data.shipping_address.country_code !== 'US') {
+                    return actions.reject();
+                }
+                return actions.resolve();
+            }
+        },
+
+        onError: function () {
+            return (err) => {
+                console.error(err);
+                window.location.href = "/your-error-page-here";
+            }
+        },
+
+        style: function () {
+            return {
+                shape: 'rect',
+                color: 'black',
+                layout: 'horizontal',
+                label: 'paypal',
+                tagline: false
+            }
+        },
+    },
+});
+
+var this_;
+const vm = new Vue({
+    el: "#container",
+
+    data: {
+        price: '12.99'
+    },
+    
+    mounted: function() {
+        this_ = this;
+        this_.get_orderID();
+    },
+
+    methods: {
+        async get_orderID() {
+            var timer = ms => new Promise(res => setTimeout(res, ms));
+            var keeplooping = true;
+            while(keeplooping) {
+                var orderID = window.localStorage.getItem('orderID');
+                if (orderID != null) {
+                    keeplooping = false;
+                }
+                else {
+                    keeplooping = true;
+                }
+                await timer(2000);
+            };
+            
+            if (orderID != null) {
+                await timer(2000);
+                this.send_orderID(orderID);
+            }
+        },
+
+        send_orderID(orderID) {
+            var datas = {};
+            var trainer_id = window.localStorage.getItem('trainer_id');
+            var totalPrice = window.localStorage.getItem('price');
+            datas.orderID = orderID;
+            datas.trainer_id = trainer_id;
+            datas.totalPrice = totalPrice;
+            var data = JSON.stringify(datas);
+
+            $.ajax({
+                headers: {
+                    Authorization: "Bearer " + localStorage.getItem("token")
+                },
+                url: "http://127.0.0.1:8000/api/orders/create/",
+                type: "POST",
+                data: data,
+                dataType: "json",
+                contentType: "application/json; charset=utf_8",
+
+                success: function(rs) {
+                    window.location.href = '/html/trainee/connection/paymentThankyou.html';
+                },
+
+                error: function(rs) {
+                    alert("Could not process your order.");
+                }
+            })
+        }
     }
-}).render('#paypal-button-container')
+});
