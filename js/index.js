0c97746da9d80653438d9796d41e04c3edd86746
SunshineNurSmile <yuanyiw@bu.edu>
2022-04-27 05:46:09 -0400

    Created warning about this site being a class project

--- js/index.js
+++ js/index.js
@@ -6,10 +6,12 @@ var app = new Vue({
 
     methods:{
         jump_to_spec() {
+            alert("This is a class project. Please don't enter any confidential information or upload any illegal files or pay with actual PayPal account.");
             window.location.href = "/html/clientSpecification.html";
         },
 
         jump_to_login() {
+            alert("This is a class project. Please don't enter any confidential information or upload any illegal files or pay with actual PayPal account.");
             window.location.href = "/html/login.html";
         }
     }
